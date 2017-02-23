d17a5ff74edd56246234704250ff2467f2f5d24f
lttb <connect@lttb.net>
2017-02-23 16:18:32 +0300

    Fix typo

--- src/init-processor.js
+++ src/init-processor.js
@@ -3,11 +3,11 @@ import postcssJs from 'postcss-js'
 import postcssrc from 'postcss-load-config'
 
 
-export default getPorcessor => postcssrc()
+export default getProcessor => postcssrc()
   .then(({ plugins, options }) => {
     const css = postcss(plugins)
 
-    getPorcessor(null, ({ from, data }) => cb =>
+    getProcessor(null, ({ from, data }) => cb =>
       css.process(data, { ...options, from })
         .then(res => cb(null, postcssJs.objectify(res.root))))
   })
